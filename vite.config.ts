cc516bc8cf37e82eca30c2eb66da4af816b57e23
hieutt03 <hieuttjob@gmail.com>
2025-03-22 13:47:31 +0700

    init deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,26 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import {defineConfig} from "vite"
+import path from 'path'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
-        },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  server: {
+    watch: {
+      usePolling: true
     },
+    host: true,
+    strictPort: true,
+    port: 5173
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    sourcemap: false
+  }
 })
