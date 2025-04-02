5f88a66ea77fccfcd32b66bd5cddac2ceff55aac
inhoang2523 <yle405960@gmail.com>
2025-04-02 22:25:03 +0700

    fix/update routing logic

--- src/components/organisms/Layout/Header.tsx
+++ src/components/organisms/Layout/Header.tsx
@@ -1,14 +1,31 @@
+import { useSelector } from "react-redux";
 import Navbar from "../Navbar/AdminNavbar"
+import { RootState } from "@/store";
+
+const getRoleName = (roleID: number) => {
+  switch (roleID) {
+    case 1:
+      return "Admin";
+    case 2:
+      return "Manager";
+    case 4:
+      return "Staff";
+    default:
+      return "User";
+  }
+};
 
 const Header = () => {
+  const user = useSelector((state: RootState) => state.auth.user);
+
   return (
     <div>
       <Navbar
         title="Dashboard"
         user={{
-          name: "John Doe",
-          role: "Admin",
-          avatar: "https://i.pinimg.com/736x/77/1b/20/771b2040dc38a0ac151c398a22af2d42.jpg"
+          name: user?.userName || "Guest",
+          role: getRoleName(user?.roleID as number),
+           avatar: user?.avatar  || "https://i.pinimg.com/736x/77/1b/20/771b2040dc38a0ac151c398a22af2d42.jpg"
         }}
         languages={[
           { code: "en", label: "English" },
--- src/components/organisms/Navbar/AdminNavbar.tsx
+++ src/components/organisms/Navbar/AdminNavbar.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BellIcon, ChevronDownIcon } from "@radix-ui/react-icons";
+import { BellIcon} from "@radix-ui/react-icons";
 import i18n from "i18next";
 
 interface NavbarProps {
@@ -24,7 +24,7 @@ const Navbar: React.FC<NavbarProps> = ({
 
   return (
     <div className="flex items-center justify-between px-6 py-3 bg-white ">
-      {title && <h2 className="text-2xl font-semibold text-gray-800">{title}</h2>}
+      {title && <h2 className="text-2xl font-semibold text-[#516D19516D19]">{title}</h2>}
 
       <div className="flex items-center gap-4">
         {languages.length > 0 && (
@@ -54,7 +54,6 @@ const Navbar: React.FC<NavbarProps> = ({
               <span className="text-sm font-medium text-gray-800">{user.name}</span>
               <small className="text-xs text-gray-500">{user.role}</small>
             </div>
-            <ChevronDownIcon className="w-5 h-5 text-gray-500" />
           </div>
         )}
       </div>
